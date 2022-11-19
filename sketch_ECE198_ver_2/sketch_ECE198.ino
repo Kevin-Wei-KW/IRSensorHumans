1432e2ffda77cedfc87e1a232c243f81f7f9f877
Matthew Andrade <117783143+MatthewlrAndrade@users.noreply.github.com>
2022-11-18 21:49:40 -0500

    Add files via upload

---
+++ sketch_ECE198_ver_2/sketch_ECE198.ino
@@ -0,0 +1,84 @@
+//Code authored by:
+
+
+int ledPin = 13;                // choose the pin for the LED
+int inputPin = 8;               // choose the input pin (for PIR sensor)
+int pirState = LOW;             // we start, assuming no motion detected
+int val = 0;                    // variable for reading the pin status
+ 
+void setup() {
+  pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(inputPin, INPUT);     // declare sensor as input
+ 
+  Serial.begin(9600);
+}
+ 
+void loop(){
+  if (pirState == HIGH){
+  val = HIGH;
+  delay(10000); //Set the timeout time
+  val = HIGH;
+  }
+
+  val = digitalRead(inputPin);  // read input value
+  
+  if (val == HIGH)	// check if the input is HIGH
+  {            
+    digitalWrite(ledPin, HIGH);  // turn LED ON
+	
+    if (pirState == LOW) 
+	{
+      Serial.println("Motion detected!");	// print on output change
+      pirState = HIGH;
+    }
+  } 
+  else 
+  {
+    digitalWrite(ledPin, LOW); // turn LED OFF
+	
+    if (pirState == HIGH)
+	{
+      Serial.println("Motion ended!");	// print on output change
+      pirState = LOW;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+/* Old not functioning code 
+bool Human_detected = 0;
+int IR_State = LOW;
+
+void setup() {
+  // put your setup code here, to run once:
+
+ //IR Sensor (on pin D2) is an input
+ pinMode(D2, INPUT);    
+ //pinMode(D13, OUTPUT);     //LED Test
+ Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ Human_detected = digitalRead(D2);
+
+  if (Human_detected){
+    Serial.println("Human Detected");
+    //digitalWrite(D13, LOW);
+
+  } else {
+    Serial.println("Human Not Detected");
+    //digitalWrite(D13, LOW);
+  }
+  
+
+}
+*/
